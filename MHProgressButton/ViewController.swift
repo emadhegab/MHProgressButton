2a7744dd64b457dd9cf8c94237bec1730ee79d61
Mohamed Emad Hegab <mohamed.hegab@zalando.de>
2017-04-19 10:56:31 +0200

    Initial Commit

---
+++ MHProgressButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MHProgressButton
+//
+//  Created by Mohamed Emad Abdalla Hegab on 19.04.17.
+//  Copyright © 2017 MohamedHegab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
